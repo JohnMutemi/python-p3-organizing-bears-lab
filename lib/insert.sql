1bb085d437bfe5fbc16b794ee55e43afcb5b3545
JohnMutemi <jmtutorsalp@gmail.com>
2024-06-05 06:05:31 +0300

    commit organizing bears

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 14, 'M', 'Brown', 'Calm', 1),
+('Rowdy', 8, 'M', 'Black', 'Aggressive', 1),
+('Tabitha', 6, 'F', 'White', 'Playful', 1),
+('Sergeant Brown', 12, 'M', 'Brown', 'Serious', 1),
+('Melissa', 7, 'F', 'Brown', 'Gentle', 1),
+('Grinch', 5, 'M', 'Green', 'Grumpy', 1),
+('Wendy', 4, 'F', 'Blonde', 'Friendly', 1),
+(NULL, 6, 'M', 'Black', 'Unknown', 1);
